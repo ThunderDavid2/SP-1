b713fcf38af73cb42739e9a4ae6744c39067a546
ThunderDavid2 <david.x.carlsson@hotmail.com>
2026-09-11 14:10:28 +0200

    Revert "Merge branch 'main' of https://github.com/ThunderDavid2/SP-1"
    This reverts commit 04d69889c8418d88c9365ed5fa14e24df043bbbc, reversing
    changes made to a6e2958eedfbc10f30f90ca75e714485ad6e8697.

--- Assets/Scripts/PlayMovement.cs
+++ Assets/Scripts/PlayMovement.cs
@@ -5,10 +5,8 @@ public class PlayMovement : MonoBehaviour
     public float runSpeed = 2;
     public float jumpSpeed = 3;
     public float doubleJumpSpeed = 2.5f;
-
+    //Hallo hej
     //Randmo
-    //Hallo hej jfjdifj
-
     private bool canDoubleJump;
 
     Rigidbody2D rd2D;
